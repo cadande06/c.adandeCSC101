c5f3059fd1f50fc2f5c3953d17f0b855ac72b3ba
Chioma Adande <chiomadande@gmail.com>
2024-10-23 20:16:19 +0100

    first commit

---
+++ week-2/practice_1.rs
@@ -0,0 +1,9 @@
+fn main(){
+    println!("Welcome to COS 101!");
+    println!("The course learning outcome is as follow:");
+    println!("1. Distinguish bettween computer and computer programming,/n
+              2. Develop some techniques in computer science,/n
+              3. Understand the different areas of study in computer science,/n
+              5. Navigate the career prospects in computer science./n
+              6. Conversant with computer programming concepts." );
+}
