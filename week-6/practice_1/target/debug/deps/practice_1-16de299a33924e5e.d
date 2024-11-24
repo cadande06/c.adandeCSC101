d54b79ce7f8d5d7dbf3be2fe5dbdae6f501f94bf
Chioma <chiomadande@gmail.com>
2024-11-24 12:54:31 +0100

    first commit

---
+++ week-6/practice_1/target/debug/deps/practice_1-16de299a33924e5e.d
@@ -0,0 +1,5 @@
+/Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_1/target/debug/deps/libpractice_1-16de299a33924e5e.rmeta: src/main.rs
+
+/Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_1/target/debug/deps/practice_1-16de299a33924e5e.d: src/main.rs
+
+src/main.rs:
---
+++ week-6/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+/Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_1/target/debug/practice_1: /Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_1/src/main.rs
