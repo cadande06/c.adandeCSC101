cec07825609c6f09752335bfa969786230089256
Chioma <chiomadande@gmail.com>
2024-11-24 14:06:45 +0100

    commit

---
+++ week-6/practice_10/target/debug/deps/practice_10-063f024f64294f2f.d
@@ -0,0 +1,5 @@
+/Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_10/target/debug/deps/practice_10-063f024f64294f2f: src/main.rs
+
+/Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_10/target/debug/deps/practice_10-063f024f64294f2f.d: src/main.rs
+
+src/main.rs:
---
+++ week-6/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+/Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_10/target/debug/practice_10: /Users/chiomadande/Documents/c.adandeCSC101/week-6/practice_10/src/main.rs
