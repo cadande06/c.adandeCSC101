9c8a6c7312153d9aeea30c4aef81a14a6b80d14f
Chioma <chiomadande@gmail.com>
2024-12-18 12:09:07 +0100

    commit

--- week-8/project_3/src/main.rs
+++ week-8/project_3/src/main.rs
@@ -11,12 +11,12 @@ fn main() {
     ["4", "Adewale Jimoh Akanbi"],
     ["5", "Osazuwa Faith Etieye"]
     ];
-    let mut names = File::create("/Users/chiomadande/Documents/c.adandeCSC101/week-8/project_2/target/names.txt").expect("failed to create file");
+    let mut names = File::create("/Users/chiomadande/Documents/c.adandeCSC101/week-8/project_3/target/names.txt").expect("failed to create file");
     writeln!(names,"{:<5}{:<30}", headn[0], headn[1]).unwrap();
     writeln!(names,"{:-<35}\n", "").unwrap();
     for n in name_list{
     writeln!(names,"{:<5}{:<30}", n[0], n[1]).unwrap();
-    writeln!(names,"{:-<35}\n", "").unwrap();
+    
     }
 
     // file 2
@@ -28,12 +28,12 @@ fn main() {
     ["4", "Power & Steel"],
     ["5", "Petroleum"]
     ];
-    let mut ministries = File::create("/Users/chiomadande/Documents/c.adandeCSC101/week-8/project_2/target/ministries.txt").expect("failed to create file");
+    let mut ministries = File::create("/Users/chiomadande/Documents/c.adandeCSC101/week-8/project_3/target/ministries.txt").expect("failed to create file");
     writeln!(ministries,"{:<5}{:<20}", headm[0], headm[1]).unwrap();
     writeln!(ministries,"{:-<25}\n", "").unwrap();
     for m in min_list{
     writeln!(ministries,"{:<5}{:<20}", m[0], m[1]).unwrap();
-    writeln!(ministries,"{:-<25}\n", "").unwrap();
+    
     }
 
     // file 3
@@ -45,11 +45,11 @@ fn main() {
     ["4", "South West"],
     ["5", "South East"]
     ];
-    let mut geos = File::create("/Users/chiomadande/Documents/c.adandeCSC101/week-8/project_2/target/geos.txt").expect("failed to create file");
+    let mut geos = File::create("/Users/chiomadande/Documents/c.adandeCSC101/week-8/project_3/target/geos.txt").expect("failed to create file");
     writeln!(geos,"{:<5}{:<25}", headg[0], headg[1]).unwrap();
     writeln!(geos,"{:-<30}\n", "").unwrap();
     for g in geo_list{
     writeln!(geos,"{:<5}{:<25}", g[0], g[1]).unwrap();
-    writeln!(geos,"{:-<30}\n", "").unwrap();
+    
     }
 }
