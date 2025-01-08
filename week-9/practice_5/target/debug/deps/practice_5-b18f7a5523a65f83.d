4ea8afc10b09b328270b999087f24af6beda4c3d
Chioma <chiomadande@gmail.com>
2025-01-08 19:55:08 +0100

    commits

---
+++ week-9/practice_5/target/debug/deps/practice_5-b18f7a5523a65f83.d
@@ -0,0 +1,5 @@
+/Users/chiomadande/Documents/c.adandeCSC101/week-9/practice_5/target/debug/deps/practice_5-b18f7a5523a65f83: src/main.rs
+
+/Users/chiomadande/Documents/c.adandeCSC101/week-9/practice_5/target/debug/deps/practice_5-b18f7a5523a65f83.d: src/main.rs
+
+src/main.rs:
---
+++ week-9/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+/Users/chiomadande/Documents/c.adandeCSC101/week-9/practice_5/target/debug/practice_5: /Users/chiomadande/Documents/c.adandeCSC101/week-9/practice_5/src/main.rs
